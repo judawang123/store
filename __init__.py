c275d9950ca2c79e276f25c1cecf5c434709ff34
judawang123 <87293289+judawang123@users.noreply.github.com>
2021-07-14 15:35:08 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,46 @@
+#判断下列变量名是否和合法
+char = 1
+print(char)
+
+Oax_li = 2
+print(Oax_li)
+
+fLul = 3
+print(fLul)
+
+BYTE = 4
+print(BYTE)
+
+T_T = 5
+print(T_T)
+
+# 有以下两个数，使用+,-实现两个数的调换
+A = 56
+B = 78
+A = A+B
+B = A-B
+A = A-B
+print(A,B)
+
+
+
+# 定义两个变量stu1和stu2，分别存储45和23，并用print()打印两个数的和
+stu1 = 45
+stu2 = 23
+print(stu1+stu2)
+
+# 定义5个变量，并用print()打印5个数的和、平均值
+A = 32
+B = 21
+C = 43
+D = 54
+E = 65
+print(A+B+C+D+E)
+print((A+B+C+D+E)/5)
+
+
+
+# 定义一个变量num1,并赋值45.然后将num1值赋值给num2。打印出来。
+num1 = 45
+num2 = num1
+print(num2)
